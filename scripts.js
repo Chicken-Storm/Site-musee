95b73cec64b44231977aa3cf4b74d046c521cb0e
Chicken-Storm <kilian.beney@gmail.com>
2023-10-19 14:50:25 +0200

    Co-authored-by: aurore.menn@sfr.fr <aurore.menn@sfr.fr> Co-authored-by: Maxence <vitagerman@users.noreply.github.com> Co-authored-by: bastien.betat64@gmail.com <bastien.betat64@gmail.com> Co-authored-by: noe.aries@mmibordeaux.com <noe.aries@mmibordeaux.com>

--- scripts.js
+++ scripts.js
@@ -41,3 +41,32 @@ function displayImages() {
   images[index-1].style.display = "block";
   setTimeout(displayImages, 6000); 
 }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Fonction pour avancer ou reculer dans les diapositives
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Fonction pour afficher une diapositive spécifique
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change l'image toutes les 2 secondes
+}
